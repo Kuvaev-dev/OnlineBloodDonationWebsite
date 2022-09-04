cb1f5e6b18411aa428f2758d08e34d0c091e5396
Kuvaev-dev <likeaboss13371@gmail.com>
2022-09-04 14:19:24 +0300

    Login View Design Added

--- BloodDonation/Controllers/HomeController.cs
+++ BloodDonation/Controllers/HomeController.cs
@@ -27,5 +27,18 @@ namespace BloodDonation.Controllers
             ViewBag.CityID = new SelectList(DB.CityTables.ToList(), "CityID", "City", "0");
             return View(registrationMV);
         }
+
+        public ActionResult Login()
+        {
+            var usermv = new UserMV();
+            return View(usermv);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserMV user)
+        {
+            return View(user);
+        }
     }
 }
