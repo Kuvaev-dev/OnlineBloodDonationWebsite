450f94ca76ccafdffafb9bb6d698f795674b1693
Kuvaev-dev <likeaboss13371@gmail.com>
2022-09-06 14:40:59 +0300

    User Profile Added

---
+++ BloodDonation/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using DatabaseLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BloodDonation.Controllers
+{
+    public class UserController : Controller
+    {
+        db_a8c2c8_blooddonationEntities DB = new db_a8c2c8_blooddonationEntities();
+
+        public ActionResult UserProfile(int? id)
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+
+            var user = DB.UserTables.Find(id);
+            return View(user);
+        }
+    }
+}
