e2710cebb4b0dd3a4a747ff11e565958444512cb
Kuvaev-dev <likeaboss13371@gmail.com>
2022-09-13 22:33:36 +0300

    Complete Seeker Request Added

--- BloodDonation/Controllers/FinderController.cs
+++ BloodDonation/Controllers/FinderController.cs
@@ -346,5 +346,28 @@ namespace BloodDonation.Controllers
 
             return View(requestList);
         }
+
+        public ActionResult CompleteRequest(int? id)
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+
+            var request = DB.RequestTables.Find(id);
+            if (request.RequestTypeID == 1)
+            {
+                var donor = DB.DonorTables.Find(request.AcceptedID);
+                donor.LastDonationDate = DateTime.Now;
+                DB.Entry(donor).State = System.Data.Entity.EntityState.Modified;
+                DB.SaveChanges();
+            }
+
+            request.RequestStatusID = 3;
+            DB.Entry(request).State = System.Data.Entity.EntityState.Modified;
+            DB.SaveChanges();
+
+            return RedirectToAction("ShowAllRequests");
+        }
     }
 }
